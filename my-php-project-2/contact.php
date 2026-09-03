e3e98b236b7196dc5d45116edf8efd758a991041
merilynak <akmerilyn@gmail.com>
2026-09-03 13:22:27 +0700

    Update PHP profile page

---
+++ my-php-project-2/contact.php
@@ -0,0 +1,2 @@
+<?php
+    echo 'This is my contact page';
--- my-php-project-2/index.php
+++ my-php-project-2/index.php
@@ -1,24 +1,6 @@
 <?php
+    $name = 'Khu';
 
-$numbers = [15.11, 20, 99.5, 40, 50];
+    echo $name;
 
-function calculateAverage($numbers) {
-    $average = array_sum($numbers) / count($numbers);
-    return $average;
-}
-
-function calculateHighest($numbers) {
-    $highest = max($numbers);
-    return $highest;
-}
-
-function calculateLowest($numbers) {
-    $lowest = min($numbers);
-    return $lowest;
-}
-
-echo "Average: " . calculateAverage($numbers) . "\n";
-echo "Highest: " . calculateHighest($numbers) . "\n";
-echo "Lowest: " . calculateLowest($numbers) . "\n";
-
-?>
+    
